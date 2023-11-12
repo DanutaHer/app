d07bb6b10722ec8296b8eb14a21b297a1a1398fe
DanutaHer <hering.danutaa@gmail.com>
2023-11-12 23:42:49 +0100

    changes

--- index.js
+++ index.js
@@ -28,11 +28,7 @@ require("./routes/billingRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-
-  const path = require("path");
-  app.get("*", (res, req) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  app.use("*", express.static("client/build")); 
 }
 
 const PORT = process.env.PORT || 5000;
